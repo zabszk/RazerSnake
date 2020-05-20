57177c114458ac1b7cf73e4f2076573a362ced30
Łukasz Jurczyk <zabszk@protonmail.ch>
2020-05-20 20:26:03 +0200

    Minor changes

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,22 +30,25 @@
         private void InitializeComponent()
         {
             this.statusLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // statusLabel
-            // 
             this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.statusLabel.Location = new System.Drawing.Point(12, 9);
             this.statusLabel.Name = "statusLabel";
             this.statusLabel.Size = new System.Drawing.Size(776, 82);
             this.statusLabel.TabIndex = 1;
             this.statusLabel.Text = "Loading";
-            // 
-            // MainForm
-            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(584, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 44);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Created by Łukasz Jurczyk\r\nBuild Date: 2020/05/20";
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 174);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.statusLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.KeyPreview = true;
@@ -58,6 +61,7 @@
             this.ResumeLayout(false);
         }
 
+        private System.Windows.Forms.Label label1;
         internal System.Windows.Forms.Label statusLabel;
 
         #endregion
