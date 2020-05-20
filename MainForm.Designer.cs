66fd7687fe84c4f1a673f540fc884b00831788d0
Łukasz Jurczyk <zabszk@protonmail.ch>
2020-05-20 20:26:29 +0200

    Minor changes

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,25 +30,34 @@
         private void InitializeComponent()
         {
             this.statusLabel = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.authorLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // statusLabel
+            // 
             this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.statusLabel.Location = new System.Drawing.Point(12, 9);
             this.statusLabel.Name = "statusLabel";
             this.statusLabel.Size = new System.Drawing.Size(776, 82);
             this.statusLabel.TabIndex = 1;
             this.statusLabel.Text = "Loading";
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(584, 120);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(204, 44);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Created by Łukasz Jurczyk\r\nBuild Date: 2020/05/20";
+            // 
+            // authorLabel
+            // 
+            this.authorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.authorLabel.ForeColor = System.Drawing.Color.Red;
+            this.authorLabel.Location = new System.Drawing.Point(584, 120);
+            this.authorLabel.Name = "authorLabel";
+            this.authorLabel.Size = new System.Drawing.Size(204, 44);
+            this.authorLabel.TabIndex = 2;
+            this.authorLabel.Text = "Created by Łukasz Jurczyk\r\nBuild Date: 2020/05/20";
+            // 
+            // MainForm
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 174);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.authorLabel);
             this.Controls.Add(this.statusLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.KeyPreview = true;
@@ -61,7 +70,7 @@
             this.ResumeLayout(false);
         }
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label authorLabel;
         internal System.Windows.Forms.Label statusLabel;
 
         #endregion
